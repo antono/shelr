8788774510a0236d0b6acf32606a1c113e8124ee
Vasiliy Ermolovich <younash@gmail.com>
2012-03-25 18:55:19 +0300

    cleanup

--- spec/shelr/recorder_spec.rb
+++ spec/shelr/recorder_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 
 describe Shelr::Recorder do
 
-  before(:each) do
+  before do
     STDIN.stub(:gets).and_return('my shellcast')
     Shelr.backend = 'script'
   end
 
   describe "#record!" do
-    before(:each) do
+    before do
       subject.stub(:system).with(anything).and_return(true)
     end
 
-    it "should start script session" do
-      subject.should_receive("system").with(Regexp.compile("script"))
+    it "starts script session" do
+      subject.should_receive(:system).with(/script/)
       subject.record!
     end
   end
